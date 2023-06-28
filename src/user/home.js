b5d68470e1ee8ac8c33d5da09a307c9e752f3695
Kamilake <exjang0@gmail.com>
2023-06-28 16:19:32 +0900

    쓰레기 추가

--- src/user/home.js
+++ src/user/home.js
@@ -4,6 +4,7 @@ import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Avatar from '@mui/material/Avatar';
+import Skeleton from '@mui/material/Skeleton';
 import BadgeAvatars from './components/BadgeAvatars';
 
 import fetchInfoApi from '../api/fetchInfoApi';
@@ -28,17 +29,28 @@ export default function home() {
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 1 }}>
-        <Header title="홈" />
-        <div className="home" style={{ whiteSpace: 'pre-wrap', WordBreak: 'break-word' }}>
+        <Header title="홈" userAvatarUrl={data.userAvatarUrl} />
+        <div className="home">
 
           <BadgeAvatars userName={data?.userEffectiveName + "님, 안녕하세요!"} avatarUrl={data.userAvatarUrl} />
           <br />
           {data?.guildId == 0 ? null : <>
             {data.userEffectiveName}님은 지금 {data.guildName} 서버의 {data.channelName} 채널에 있어요!
-          </>}
+          </>} <br></br>
           <Button variant="contained" color="primary" onClick={gohome}> 설정으로 이동 </Button>
           {/*  */}
         </div>
+        {/* For variant="text", adjust the height via font-size */}
+        <br />
+        <Box>
+          <Skeleton variant="text" sx={{ fontSize: '1rem' }} animation="wave" /> <br />
+
+          {/* For other variants, adjust the size with `width` and `height` */}
+          <Skeleton variant="circular" width={60} height={60} animation="wave" /> <br />
+          <Skeleton variant="rounded" height={40} animation="wave" /> <br />
+          <Skeleton variant="rounded" height={70} animation="wave" /> <br />
+          <Skeleton variant="rounded" height={60} animation="wave" /> <br />
+        </Box>
         <ProTip />
         <Molu />
       </Box>
