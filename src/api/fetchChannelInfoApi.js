4ce2d481a454f5b6eb2868ee12d1827f467aa7df
Kamilake <exjang0@gmail.com>
2023-06-28 16:16:54 +0900

    채널 정보 가져오는 API

---
+++ src/api/fetchChannelInfoApi.js
@@ -0,0 +1,26 @@
+// fetchChannelInfoApi.js
+import useFetch from "./useFetch";
+
+const fetchChannelInfoApi = ({channelId}) => {
+  let { data, loading, error } = useFetch(
+    'https://kamibot.kami.live/api/channel?channelId=' + channelId
+  );
+  
+
+  if (loading) {
+    //예제 json 데이터로 대신 표시
+    data =
+    {
+      "name": "채널이름",
+      "timeCreated": "2023-02-20T07:53:57.214Z",
+      "llm": false,
+      "type": "TEXT",
+      "channelId": "1077136010883907696",
+      "minicuda": true
+    };
+
+  }
+  return { data, loading, error };
+};
+
+export default fetchChannelInfoApi;
