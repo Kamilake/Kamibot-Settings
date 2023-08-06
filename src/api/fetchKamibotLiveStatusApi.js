7489784a7454fcedae8f50706d71e35b8152ff49
Kamilake <exjang0@gmail.com>
2023-08-07 08:40:45 +0900

    실시간 상태 API 만들기

---
+++ src/api/fetchKamibotLiveStatusApi.js
@@ -0,0 +1,24 @@
+// fetchChannelListApi.js
+import useFetch from "./useFetch";
+
+const fetchKamibotLiveStatusApi = () => {
+
+  let { data, loading, error } = useFetch(
+    'https://kamibot.kami.live/api/liveStatus'
+  );
+
+  if (loading) {
+    //예제 json 데이터로 대신 표시
+    data = [
+      { channelName: 'The Shawshank Redemption', channelType: "PRIVATE", channelId: 1994, categoryName: "일반" },
+      { channelName: 'The Godfather', channelType: "PRIVATE", channelId: 1972, categoryName: "일반" },
+      { channelName: 'The Godfather: Part II', channelType: "PRIVATE", channelId: 1974, categoryName: "일반a" },
+      { channelName: 'The Dark Knight', channelType: "PRIVATE", channelId: 2008, categoryName: "일반b" },
+      { channelName: '12 Angry Men', channelType: "PRIVATE", channelId: 1957, categoryName: "일반b" },
+    ];
+
+  }
+  return { data, loading, error };
+};
+
+export default fetchKamibotLiveStatusApi;
