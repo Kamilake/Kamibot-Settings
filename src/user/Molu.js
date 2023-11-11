84f8a98fb40b1986f0df9b788b4d303c6099b10e
Kamilake <exjang0@gmail.com>
2023-11-11 13:48:26 +0900

    크레딧 한글화

--- src/user/Molu.js
+++ src/user/Molu.js
@@ -6,7 +6,7 @@ export default function Molu() {
     <Typography variant="body2" color="text.secondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://mui.com/">
-        몰?루
+        Kamilake
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
--- src/user/ProTip.js
+++ src/user/ProTip.js
@@ -15,8 +15,7 @@ export default function ProTip() {
   return (
     <Typography sx={{ mt: 6, mb: 3 }} color="text.secondary">
       <LightBulbIcon sx={{ mr: 1, verticalAlign: 'middle' }} />
-      Pro tip: See more <Link href="https://mui.com/getting-started/templates/">templates</Link> on
-      the MUI documentation.
+      보너스 팁: 이 설정 창의 UI는 <Link href="https://mui.com/getting-started/templates/">MUI</Link>로 만들어졌어요!
     </Typography>
   );
 }
