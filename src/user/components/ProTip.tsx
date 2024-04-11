d6e6b221ab6530a5d8c16132bbc301dd2c96af2c
Kamilake <exjang0@gmail.com>
2024-04-11 13:54:25 +0900

    Pro Tip이 랜덤하게 나오도록 수정

--- src/user/components/ProTip.tsx
+++ src/user/components/ProTip.tsx
@@ -1,21 +1,33 @@
 import * as React from 'react';
+import { useState, useEffect, ReactElement } from 'react';
 import Link from '@mui/material/Link';
-import SvgIcon, { SvgIconProps } from '@mui/material/SvgIcon';
 import Typography from '@mui/material/Typography';
-
-function LightBulbIcon(props: SvgIconProps) {
-  return (
-    <SvgIcon {...props}>
-      <path d="M9 21c0 .55.45 1 1 1h4c.55 0 1-.45 1-1v-1H9v1zm3-19C8.14 2 5 5.14 5 9c0 2.38 1.19 4.47 3 5.74V17c0 .55.45 1 1 1h6c.55 0 1-.45 1-1v-2.26c1.81-1.27 3-3.36 3-5.74 0-3.86-3.14-7-7-7zm2.85 11.1l-.85.6V16h-4v-2.3l-.85-.6C7.8 12.16 7 10.63 7 9c0-2.76 2.24-5 5-5s5 2.24 5 5c0 1.63-.8 3.16-2.15 4.1z" />
-    </SvgIcon>
-  );
-}
+import LightBulbIcon from '@mui/icons-material/LightbulbOutlined';
 
 const ProTip: React.FC = () => {
+  const tips: ReactElement[] = [
+    <>이 설정 창의 UI는 <Link href="https://mui.com/getting-started/templates/">MUI</Link>로 만들어졌어요!</>,
+    <>AI 카미봇에게 "내일 10시 유닉스 타임스탬프 만들어줘" 라고 해 보세요.</>,
+    <>AI 툴킷은 그림뿐만 아니라 동영상과 파일도 다룰 수 있어요.</>,
+    <>이 설정 창의 UI는 <Link href="https://github.com/Kamilake/Kamibot-Settings">오픈소스로 공개</Link>되어 있어요!</>,
+    <>카미봇의 모든 기능은 무료이며, 모든 비용은 카미가 내고 있어요.</>,
+    <>여러분은 자신의 TTS 보이스를 변경할 수 있어요. <Link href={`/user/personal${window.location.search}`}>여기에서요!</Link></>,
+    <>어떤 보너스 팁을 추가해볼까요?</>,
+    <>어떤 보너스 팁을 추가해볼까요?</>,
+    // 추가 팁들...
+  ];
+
+  const [tip, setTip] = useState<ReactElement | null>(null);
+
+  useEffect(() => {
+    const randomTip = tips[Math.floor(Math.random() * tips.length)];
+    setTip(randomTip);
+  }, []);
+
   return (
     <Typography sx={{ mt: 6, mb: 3 }} color="text.secondary">
       <LightBulbIcon sx={{ mr: 1, verticalAlign: 'middle' }} />
-      보너스 팁: 이 설정 창의 UI는 <Link href="https://mui.com/getting-started/templates/">MUI</Link>로 만들어졌어요!
+      보너스 팁: {tip}
     </Typography>
   );
 }
