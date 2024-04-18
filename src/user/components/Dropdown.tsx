168ca8792c9ca0a7a5762434d8a8d52d9ebf9625
Kamilake <exjang0@gmail.com>
2024-04-18 11:06:08 +0900

    드롭다운 컴포넌트에 도움말 모달 추가

--- src/user/components/Dropdown.tsx
+++ src/user/components/Dropdown.tsx
@@ -1,34 +1,69 @@
-import React from 'react';
-import { Grid, InputLabel, FormControl, Select, MenuItem, SelectChangeEvent } from '@mui/material';
+import React, { useState, ReactNode } from 'react';
+import { Grid, InputLabel, FormControl, Select, MenuItem, SelectChangeEvent, IconButton, Tooltip, Modal, Box, Typography, Button, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from '@mui/material';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 
 interface DropdownProps {
   label: string;
   value: string | number;
   onChange: (event: SelectChangeEvent<any>) => void;
   items: { value: string | number; text: string; disabled?: boolean }[];
+  help?: ReactNode;
 }
 
-const Dropdown: React.FC<DropdownProps> = ({ label, value, onChange, items }) => (
-  <Grid container spacing={2} alignItems="center" direction="row" marginTop={1} marginBottom={1}>
-    <Grid item xs={12} sm={5}>
-      <InputLabel id={`${label}-label`}>{label}</InputLabel>
+const Dropdown: React.FC<DropdownProps> = ({
+  label,
+  value,
+  onChange,
+  items,
+  help
+}) => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <Grid container spacing={2} alignItems="center" direction="row" marginTop={1} marginBottom={1}>
+      <Grid item xs={12} sm={5}>
+        <Grid container alignItems="center">
+          <InputLabel id={`${label}-label`}>{label}</InputLabel>
+          {help && (
+            <Tooltip title="Help">
+              <IconButton size="small" onClick={handleOpen}>
+                <HelpOutlineIcon />
+              </IconButton>
+            </Tooltip>
+          )}
+        </Grid>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <FormControl fullWidth>
+          <Select
+            labelId={`${label}-label`}
+            value={value}
+            onChange={onChange}
+          >
+            {items.map((item) => (
+              <MenuItem key={item.value} value={item.value} disabled={item.disabled}>
+                {item.text}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Grid>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>도움말</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            {help}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>알았어요!</Button>
+        </DialogActions>
+      </Dialog>
     </Grid>
-    <Grid item xs={12} sm={6}>
-      <FormControl fullWidth>
-        <Select
-          labelId={`${label}-label`}
-          value={value}
-          onChange={onChange}
-        >
-          {items.map((item) => (
-            <MenuItem key={item.value} value={item.value} disabled={item.disabled}>
-              {item.text}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    </Grid>
-  </Grid>
-);
+  );
+};
 
 export default Dropdown;
