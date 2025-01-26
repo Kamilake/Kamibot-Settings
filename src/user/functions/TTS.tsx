b29aa954e0f284d92a17cabd68df2deb4e920ef3
Kamilake <exjang0@gmail.com>
2025-01-26 19:22:56 +0900

    TTS 설정 기능 개선 및 상태 관리 통합

--- src/user/functions/TTS.tsx
+++ src/user/functions/TTS.tsx
@@ -1,10 +1,12 @@
-import React from "react";
-import { HelpOutline, NoPhotography, PersonAdd, PersonRemove, PhotoCamera, RecordVoiceOver, ScreenShare, StopScreenShare, VideogameAsset, VideogameAssetOff } from '@mui/icons-material';
+import React, { ChangeEvent } from "react";
+import { HelpOutline, NoPhotography, PersonAdd, PersonRemove, PhotoCamera, RecordVoiceOver, ScreenShare, StopScreenShare, VideogameAsset, VideogameAssetOff, VolumeOff } from '@mui/icons-material';
 import { FunctionInterface } from "../components/GuildSettingsGrid";
 import { Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Divider, IconButton, List, ListItemButton, ListItemIcon, ListItemText, ListSubheader, SelectChangeEvent, Switch, Tooltip, Typography } from "@mui/material";
 import setGuildFuncApi from "../../api/setGuildFuncApi";
 import fetchGuildFuncApi from "../../api/fetchGuildFuncApi";
 import DropdownLabel from "../components/DropdownLabel";
+import SwitchLabel from "../components/SwitchLabel";
+import TextboxLabel from "../components/TextboxLabel";
 // TTS 관련 설정
 
 // 음성 채널 상호작용 읽어주기
@@ -44,35 +46,31 @@ const listItemData: ListItemData[] = [
 const FunctionBody: React.FC = () => {
   const [switchStates, setSwitchStates] = React.useState<Record<string, boolean>>({});
   const { data, loading, error } = fetchGuildFuncApi('tts');
-  const [notificationStyle, setNotificationStyle] = React.useState(''); // 알림
-  const [idleTimeout, setIdleTimeout] = React.useState(-1); // 미사용 자동 꺼짐 시간
-  const [messageAutoDeleteTime, setMessageAutoDeleteTime] = React.useState(-1); // 메세지 자동삭제 시간
-  const handleNotificationStyleChange = (event: SelectChangeEvent<string>) => {
-    setGuildFuncApi(
-      'tts',
-      { 'notification_style': event.target.value as string },
-      (data) => {
-        setNotificationStyle(event.target.value as string);
-      },);
-  };
-  const handleIdleTimeoutChange = (event: SelectChangeEvent<number>) => {
-    // setIdleTimeout(event.target.value as number); 
-    setGuildFuncApi(
-      'tts',
-      { 'idle_timeout': event.target.value as number },
-      (data) => {
-        setIdleTimeout(event.target.value as number);
-      },);
-  };
 
-  const handleMessageAutoDeleteTimeChange = (event: SelectChangeEvent<number>) => {
-    // setMessageAutoDeleteTime(event.target.value as number);
+  const [state, setState] = React.useState({
+    notification_style: 'global', // 알림
+    idle_timeout: -1, // 미사용 자동 꺼짐 시간
+    read_voice_channel_member_only: false, // 음성 채널에 연결된 멤버의 메세지만 읽기
+    pronunciation_correction : false, // 'ㄲ' 를 끼역 대신 쌍기역으로 읽기
+    message_auto_delete_time: -1, // 메세지 자동삭제 시간
+    read_emoji: 'custom_only', // 이모지 읽기
+    read_sticker: true, // 스티커 읽기
+    read_url: 'never', // URL 읽기
+    read_codeblock: true, // 코드블록 읽기
+  });
+
+
+  const handleChange = (key: string, value: any) => {
     setGuildFuncApi(
       'tts',
-      { 'message_auto_delete_time': event.target.value as number },
-      (data) => {
-        setMessageAutoDeleteTime(event.target.value as number);
-      },);
+      { [key]: value },
+      () => {
+        setState((prevState) => ({
+          ...prevState,
+          [key]: value,
+        }));
+      }
+    );
   };
 
   const [accessibilityOpen, setAccessibilityOpen] = React.useState(false);
@@ -96,8 +94,10 @@ const FunctionBody: React.FC = () => {
           return acc;
         }, {} as Record<string, boolean>);
       });
-      setNotificationStyle(data.notification_style);
-      setIdleTimeout(data.idle_timeout);
+      setState((prevState) => ({
+        ...prevState, // 나중에 제거해야함
+        ...data
+      }));
       console.log(data);
     }
   }, [data, loading]);
@@ -137,8 +137,9 @@ const FunctionBody: React.FC = () => {
         help={<>/tts 명령은 카미봇의 TTS 서비스를 시작하거나 종료하는데 사용해요.<br /><br />
           이 메세지가 대화 기록에 남는 것이 지저분해 보인다면 여기서 설정할 수 있어요.<br /><br />
           하지만 다른 사람들이 어느 채널에서 TTS가 작동 중인지 확인할 수 있도록 모두에게 표시하게 설정하는 것을 권장해요.</>}
-        value={notificationStyle || 'global'}
-        onChange={handleNotificationStyleChange}
+        value={state.notification_style}
+        // onChange={handleNotificationStyleChange}
+        onChange={e => handleChange('notification_style', e.target.value)}
         items={[
           { value: 'global', text: '모두에게 표시' },
           { value: 'local', text: '나에게만 표시', disabled: false },
@@ -149,28 +150,91 @@ const FunctionBody: React.FC = () => {
       />
       <DropdownLabel
         label="미사용 자동 꺼짐 시간"
-        value={idleTimeout || 0}
-        onChange={handleIdleTimeoutChange}
+        value={state.idle_timeout}
+        onChange={e => handleChange('idle_timeout', e.target.value)}
         items={[
           { value: 0, text: '즉시', disabled: true },
           { value: 10, text: '10초', disabled: true },
           { value: 60, text: '1분', disabled: true },
+          { value: -1, text: '사용 안함' },
+        ]}
+      />
+      <SwitchLabel
+        label="음성 채널에 연결된 멤버의 메세지만 읽기"
+        checked={state.read_voice_channel_member_only}
+        onChange={e => handleChange('read_voice_channel_member_only', e.target.checked)}
+        disabled={false}
+      />
+      <SwitchLabel
+        label="'ㄲ' 를 끼역 대신 쌍기역으로 읽기"
+        checked={false}
+        onChange={e => handleChange('pronunciation_correction', e.target.checked)}
+        disabled={false}
+      />
+      <DropdownLabel
+        label="메세지 자동 청소"
+        help={<>TTS가 메세지를 읽어주면, 여러 사람들이 대화한 메세지가 기록에 남게 돼요.<br /><br />
+          그 메세지를 자동으로 카미봇이 청소해줄 수 있어요. 깔끔한 채널을 만들어봐요!</>}
+        value={state.message_auto_delete_time}
+        onChange={e => handleChange('message_auto_delete_time', e.target.value)}
+        items={[
+          { value: 0, text: '바로 청소', disabled: true },
+          { value: 10, text: '10초 뒤', disabled: true },
+          { value: 60, text: '1분 뒤', disabled: true },
+          { value: 300, text: '5분 뒤', disabled: true },
+          { value: 600, text: '10분 뒤', disabled: true },
+          { value: 1800, text: '30분 뒤', disabled: true },
+          { value: 3600, text: '1시간 뒤', disabled: true },
+          { value: 86400, text: '하루 뒤', disabled: true },
+          { value: 604800, text: '일주일 뒤', disabled: true },
           { value: -1, text: '사용 안함' },
         ]}
       />
       <DropdownLabel
-        label="메세지 청소 시간"
-        help={<>TTS가 메세지를 읽어주면, 여러 사람들이 대화한 메세지가 기록에 남게 돼요.<br /><br />
-          그 메세지를 자동으로 카미봇이 청소해줄 시간을 정할 수 있어요. 깔끔한 채널을 만들어봐요!</>}
-        value={messageAutoDeleteTime || 0}
-        onChange={handleMessageAutoDeleteTimeChange}
+        label="이모지 읽기"
+        help={<>이모지를 읽어주는 방법을 설정할 수 있어요.<br /><br />
+          - '사용 안함': 기본 이모지와 커스텀 이모지를 무시하기<br />
+          - '기본 이모지만': 기본 이모지(유니코드 이모지)만 읽기<br />
+          - '모두 읽기': TTS로 읽을 수 있는 모든 이모지를 읽기<br /><br />
+          이모지를 읽어주는 것이 불편하다면 '사용 안함'으로 설정하세요.</>}
+        value={state.read_emoji}
+        onChange={e => handleChange('read_emoji', e.target.value)}
         items={[
-          { value: 0, text: '즉시', disabled: true },
-          { value: 10, text: '10초', disabled: true },
-          { value: 60, text: '1분', disabled: true },
-          { value: -1, text: '사용 안함' },
+          { value: 'never', text: '사용 안함', disabled: false },
+          { value: 'unicode_only', text: '기본 이모지만', disabled: false },
+          { value: 'custom_only', text: '커스텀 이모지만', disabled: false },
+          { value: 'always', text: '모두 읽기', disabled: false },
         ]}
       />
+      <SwitchLabel
+        label="스티커 읽기"
+        checked={state.read_sticker}
+        // onChange={handleReadStickerChange}
+        onChange={e => handleChange('read_sticker', e.target.checked)}
+        disabled={false}
+      />
+      <DropdownLabel
+        label="URL 읽기"
+        help={<>'https://youtu.be/jNQXAC9IVRw' 를 채팅에 입력했다면...<br /><br />
+          - 사용 안함: <br />
+          - 제목 읽기: "Me at the zoo"<br />
+          - URL 읽기: "에이치티티피에스콜론슬래시슬래시와오유티유점비이슬래시제이엔큐엑스에이씨구아이브이알더블유"<br /><br /></>}
+        value={state.read_url}
+        onChange={e => handleChange('read_url', e.target.value)}
+        items={[
+          { value: 'never', text: '사용 안함', disabled: true },
+          { value: 'read_title', text: '제목 읽기', disabled: true },
+          { value: 'read_url', text: 'URL 읽기', disabled: true },
+        ]}
+      />
+
+      <SwitchLabel
+        label="코드블록 읽기"
+        checked={state.read_codeblock}
+        onChange={e => handleChange('read_codeblock', e.target.checked)}
+        disabled={false}
+      />
+
       <br />
       <Box display="flex" alignItems="center" flexDirection="row">
         <Typography variant="h5" component="span">
