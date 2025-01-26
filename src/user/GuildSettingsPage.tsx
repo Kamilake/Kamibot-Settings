9eb4a9b8cac8ec0c088f829be62b754e3caf8645
Kamilake <exjang0@gmail.com>
2025-01-26 19:23:37 +0900

    TwemojiText 컴포넌트를 추가하여 서버 이름에 이모지를 적용

--- src/user/GuildSettingsPage.tsx
+++ src/user/GuildSettingsPage.tsx
@@ -13,6 +13,7 @@ import Molu from './components/Molu';
 import fetchUserInfoApi from '../api/fetchUserInfoApi';
 import myDrawer from './components/drawer';
 import GridButton from './components/GuildSettingsGrid';
+import TwemojiText from '../../utils/twemojiUtil/TwemojiText';
 
 const Settings: React.FC = () => {
   const navigate = useNavigate();
@@ -28,15 +29,11 @@ const Settings: React.FC = () => {
     <Container maxWidth="md">
       <Box sx={{ my: 1 }}>
         <Header title="서버 설정" userAvatarUrl={user.userAvatarUrl} />
-        <Divider />
-        <br />
         <Typography variant="h4" gutterBottom component="div">
-          {loading ? `서버` : user.guildName} 설정
+          <TwemojiText>{loading ? `서버` : user.guildName}</TwemojiText> 설정
         </Typography>
         <Divider />
-        아직 만들고 있는 기능이 많아서 모두 작동하는 건 아니에요. 하지만 작동하는 건 마음껏 사용하세요!<br />
-        오작동이나 추가하면 좋을 만한 걸 발견한다면 언제든지 Kamilake에게 알려주세요! <br />
-        <br /><br />
+        <br />
         <GridButton></GridButton>
         <ProTip />
         <Molu />
